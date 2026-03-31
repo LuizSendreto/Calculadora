5b279f3fa5f8ab7341a3b1d0bdd95f054f382fcd
LuizSendreto <luiz_s_dos-santos@estudante.sesisenai.org.br>
2026-03-31 18:25:33 -0300

    add - adicionei as funções no js

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,21 @@
-function appendToDisplay() {
-
+function appendToDisplay(value) {
+    document.getElementById('display').value += value
 }
 
-function calcular(){
-    
+function clearDisplay() {
+    document.getElementById('display').value = ''
+}
+
+function calcular() {
+    const display = document.getElementById('display')
+    try {
+        display.value = eval(display.value)
+    } catch (error) {
+        display.value = 'Errado'
+    }
+}
+
+function apagar () {
+    const display = document.getElementById('display')
+    display.value = display.value.slice(0, -1)
 }
